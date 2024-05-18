cca2b0eaff806793c014e7266996561e630db116
mckaya35 <mstf.kayya@gmail.com>
2024-05-18 14:34:37 +0300

    Update 0rbit-Price-Feed.lua

--- price-feed-bot-mckaya35/0rbit-Price-Feed.lua
+++ price-feed-bot-mckaya35/0rbit-Price-Feed.lua
@@ -15,7 +15,7 @@ function handleError(msg, errorMessage)
     })
 end
 
-Handlers.add("https://github.com/mckaya35",
+Handlers.add("mckaya35",
     Handlers.utils.hasMatchingTag("Action", "Sponsored-Get-Request"),
     function(msg)
         local token = msg.Tags.Token
